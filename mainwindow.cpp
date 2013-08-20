66ad3bfe3da8a230d0d2ecc6d65db74142e09e6f
Matteo Gazzetta <gazza.matte@gmail.com>
2013-08-20 13:32:59 +0200

    menuBar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,5 @@
+#include <QMessageBox>
+#include <QFileDialog>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -6,9 +8,78 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    createActions();
+
+    //readSettings();
+    //setWindowIcon(QIcon(":/images/icon.png"));
+    //setCurrentFile("");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::createActions()
+{
+ aboutQtAction = new QAction(tr("About &Qt"), this);
+    aboutQtAction->setStatusTip(tr("Show the Qt library's About box"));
+    connect(aboutQtAction, SIGNAL(triggered()), qApp, SLOT(aboutQt()));
+}
+
+
+void MainWindow::createToolBars()
+{
+    fileToolBar = addToolBar(tr("&File"));
+    fileToolBar->addAction(newAction);
+//    fileToolBar->addAction(openAction);
+//    fileToolBar->addAction(saveAction);
+}
+
+
+void MainWindow::open()
+{
+    if (okToContinue()) {
+        QString fileName = QFileDialog::getOpenFileName(this,
+                                                        tr("Open Image"), ".",
+                                                        tr("Image files (*.png)"));
+        if (!fileName.isEmpty()){
+            // loadFile(fileName);}
+        }
+    }
+}
+
+bool MainWindow::okToContinue()
+{
+    if (isWindowModified()) {
+        int r = QMessageBox::warning(this, tr("Image"),
+                                     tr("The document has been modified.\n"
+                                        "Do you want to save your changes?"),
+                                     QMessageBox::Yes | QMessageBox::No
+                                     | QMessageBox::Cancel);
+        if (r == QMessageBox::Yes) {
+            //   return save();
+        } else if (r == QMessageBox::Cancel) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void MainWindow::on_actionNew_triggered()
+{
+    if (okToContinue()) {
+        //->clear();
+        //setCurrentFile("");
+    }
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    close();
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    open();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -14,9 +14,49 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private slots:
+    void open();
+//    bool save();
+//    bool saveAs();
+//    void about();
+//    void updateStatusBar();
+//    void imageModified();
     
+    void on_actionNew_triggered();
+
+    void on_actionExit_triggered();
+
+    void on_actionOpen_triggered();
+
 private:
     Ui::MainWindow *ui;
+
+    void createActions();
+    void createMenus();
+    void createContextMenu();
+    void createToolBars();
+    void createStatusBar();
+    void readSettings();
+    void writeSettings();
+    bool okToContinue();
+    bool loadFile(const QString &fileName);
+    bool saveFile(const QString &fileName);
+    void setCurrentFile(const QString &fileName);
+
+    QMenu *fileMenu;
+
+    QToolBar *fileToolBar;
+
+    QAction *newAction;
+    QAction *openAction;
+    QAction *saveAction;
+    QAction *saveAsAction;
+    QAction *exitAction;
+    QAction *aboutQtAction;
+
+    QString curFile;
+
 };
 
 #endif // MAINWINDOW_H
